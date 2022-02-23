0abd3b431e2768c123388616a5175c1f4ecfcdce
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-02-22 23:25:28 -0300

    Countdown function, state manipulation, date manipulation

--- countdown-timer/src/App.js
+++ countdown-timer/src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect,  useState } from 'react';
+import './app.css'
 
 function App() {
+  const [newYears, setNewYears] = useState('1 Jan 2023');
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+  const [totalSeconds, setTotalSeconds] = useState((newYearsDate - currentDate) / 1000);
+  const [days, setDays] = useState(Math.floor(totalSeconds / 3600 / 24));
+  const [hours, setHours] = useState(Math.floor(totalSeconds / 3600) % 24);
+  const [minutes, setMinutes] = useState(Math.floor(totalSeconds / 60) % 60);
+  const [seconds, setSeconds] = useState(Math.floor(totalSeconds) % 60);
+  const [count, setCount] = useState(0);
+
+  const [nameEvent, setNameEvent] = useState('New Years Eve');
+
+  function countdown(){    
+    setTotalSeconds((newYearsDate - currentDate) / 1000)
+    setDays(Math.floor(totalSeconds / 3600 / 24));
+    setHours(Math.floor(totalSeconds / 3600) % 24);
+    setMinutes(Math.floor(totalSeconds / 60) % 60);
+    setSeconds(Math.floor(totalSeconds) % 60); 
+  }
+
+  function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+  }
+
+  function handleChangeNameEvent(event){
+    setNameEvent(event.target.value);
+  }
+  function handleChangeDate(event){
+    setNewYears(event.target.value);
+  }
+
+  useEffect(() => {    
+    const interval = setInterval(() => {
+      countdown()
+      setCount(count + 1);
+    }, 1000);
+    return () => clearInterval(interval);    
+  }, [count]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> {nameEvent}</h1>
+      <div className="countdown-container">
+        <div className="countdown-element">
+          {formatTime(days)}
+          <p className="big-text" id="days">
+            <span>days</span>
+          </p>
+        </div>
+        <div className="countdown-element">
+          {formatTime(hours)}
+          <p className="big-text" id="hours">
+            <span>hours</span>
+          </p>
+        </div>
+        <div className="countdown-element">
+          {formatTime(minutes)}
+          <p className="big-text" id="mins">
+            <span>mins</span>
+          </p>
+        </div>
+        <div className="countdown-element">
+          {formatTime(seconds)}
+          <p className="big-text" id="seconds">
+            <span>seconds</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="input-container">
+        <div className="date-input">
+          <span>Write the date you want to countdown eg. (1 Jan 2023)</span>
+          <input type="text" value={newYears} onChange={handleChangeDate}>
+          </input>
+        </div>
+        <div className="description-input">
+          <span>What's the name of the event on this date? </span>
+          <input type="text" value={nameEvent} onChange={handleChangeNameEvent}>
+          </input>
+        </div>
+      </div>
+      
     </div>
   );
 }
